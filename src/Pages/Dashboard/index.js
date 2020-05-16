7d0ccf021e6bc808159ef8ed475130f7c1f647db
Your Name <you@example.com>
2020-05-16 19:18:25 -0300

    Delete Option

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,18 +1,59 @@
-import React, { useState } from 'react';
-import { Title,Form,UserInfos } from './styled';
+import React, { useState,useEffect } from 'react';
+import { Title,Form,UserInfos, Actions } from './styled';
+import { FaTrash } from "react-icons/fa";
 import api from '../../services/api';
 
 function Dashboard() {
 
   const [users, setUsers] = useState("");
-  const [usersInfo, setUsersInfo] = useState();
+  const [usersInfo, setUsersInfo] = useState([]);
+
+  useEffect(()=> {
+    if(usersInfo !== []){
+      const userStorage = localStorage.getItem("@users");
+      const parseUser = JSON.parse(userStorage);
+      setUsersInfo(parseUser);
+    }
+  },[]);
+
 
   async function handleSearchUser(e) {
     e.preventDefault();
     const response = await api.get(`/users/${users}`);
     console.log(response);
-    setUsersInfo(response.data);
+    let parseUser
+    if(usersInfo) {
+      setUsersInfo([... usersInfo, response.data]);
+      parseUser = JSON.stringify([... usersInfo, response.data]);
+      localStorage.setItem("@users", parseUser);
+
+    } else {
+      setUsersInfo([response.data]);
+      parseUser = JSON.stringify([response.data]);
+      localStorage.setItem("@users", parseUser);
+    }
+
+    setUsers("");
+
   }
+
+  function handleDelete(id) {
+    const userIndex = usersInfo.findIndex((e, i) => {
+      if (e.id === e) {
+        return i;
+      }
+    })
+
+    usersInfo.splice(userIndex, 1);
+
+    let parseUser = JSON.stringify([...usersInfo])
+    localStorage.setItem('@users', parseUser);
+    setUsersInfo([...usersInfo]);
+
+    console.log('Usuarios', usersInfo);
+    console.log('id', id);
+  }
+
   return (
     <>
       <Title>Git App </Title>
@@ -25,17 +66,20 @@ function Dashboard() {
         />
         <button><i className="fas fa-search"></i></button>
       </Form>
-      {usersInfo && (
-        <UserInfos>
-          <a href={usersInfo.html_url}>
-            <img src={usersInfo.avatar_url} alt="Foto do usuário" />
+      {usersInfo && usersInfo.map(userInfo =>
+        <UserInfos key={userInfo.id}>
+          <a href={userInfo.html_url}>
+            <img src={userInfo.avatar_url} alt="Foto do usuário" />
             <div>
               <p>
-                <strong>{usersInfo.name}</strong> /{usersInfo.login}
+                <strong>{userInfo.name}</strong> /{userInfo.login}
               </p>
-              <p>{usersInfo.bio}</p>
+              <p>{userInfo.bio}</p>
             </div>
           </a>
+          <Actions>
+            <FaTrash onClick={() => handleDelete(userInfo.id)} />
+          </Actions>
         </UserInfos>
       )}
     </>
--- src/Pages/Dashboard/styled.js
+++ src/Pages/Dashboard/styled.js
@@ -1,14 +1,16 @@
-import styled from 'styled-components';
-import { shade } from 'polished';
+import styled from "styled-components";
+import { shade, lighten } from "polished";
 
-export const Title = styled.h1 `
+export const Title = styled.h1`
+  font-size: 3.5em;
   font-weight: 700;
-`
+`;
 
-export const Form = styled.form `
-  display: flex;
+export const Form = styled.form`
   margin-top: 40px;
   max-width: 700px;
+
+  display: flex;
 
   input {
     flex: 1;
@@ -26,38 +28,66 @@ export const Form = styled.form `
     width: 150px;
     border: 0;
     border-radius: 0 5px 5px 0;
-    background: #944acb;
-    color: white;
-    transition: .2s;
+    background: #9400b4;
+    color: #fff;
+    transition: 0.2s;
 
     &:hover {
-      background: ${shade(0.2, '#944acb')}
+      background: ${shade(0.2, "#9400b4")};
     }
   }
+`;
 
-`
 export const UserInfos = styled.div`
   max-width: 700px;
-  background: #fff;
-  display: block;
   margin-top: 30px;
-  padding: 20px;
-  border-radius: 5px;
+  display: flex;
 
-  a {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
+  &:hover {
+    transform: translateX(10px);
+    transition: transform 0.2s;
   }
 
-  p {
-    padding: 0 15px;
-    color: #ccc;
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
   }
 
   img {
-    height: 90px;
     width: 90px;
+    height: 90px;
     border-radius: 50%;
   }
-`
+
+  div {
+    margin: 0 16px;
+    flex: 1;
+
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 15px;
+      color: #a8a8b3;
+      margin-top: 4px;
+    }
+  }
+`;
+
+export const Actions = styled.section`
+  background: #fff;
+  margin: 0;
+  padding: 10px 10px;
+  border-radius: 0 5px 5px 0;
+
+`;
